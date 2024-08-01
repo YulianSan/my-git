2bc041b0c3fa1c474db1a1481a5d3087b071e0a9
YulianSan <knoob0733@gmail.com>
2024-07-31 23:10:16 -0300

    copy code jonhoo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use core::panic;
+use std::ffi::CStr;
+use anyhow::{anyhow, Context};
 use flate2::read::ZlibDecoder;
 use flate2::{write::ZlibEncoder, Compression};
 use sha1::{Digest, Sha1};
@@ -6,66 +8,79 @@ use sha1::{Digest, Sha1};
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
-use std::io::prelude::*;
-use std::str;
+use std::io::{prelude::*, BufReader};
+use clap::{Parser, Subcommand};
 
-fn main() {
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Init,
+    CatFile {
+        #[clap(short = 'p')]
+        pretty_print: bool,
+        object_hash: String
+    }
+}
+
+enum Kind {
+    Blob
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // Uncomment this block to pass the first stage
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else if args[1] == "add" && !args[2].is_empty() {
-        let content = match fs::read_to_string(&args[2]) {
-            Ok(content) => content,
-            Err(_) => panic!("No can read file"),
-        };
-
-        let blob_object = format!("blob {}\0{}", content.len(), content);
-
-        let mut hasher = Sha1::new();
-        hasher.update(blob_object.clone().into_bytes());
-        let blob_object_hash: String = format!("{:x}", hasher.finalize());
-        let mut content_object = ZlibEncoder::new(vec![], Compression::default());
-        content_object
-            .write_all(blob_object.into_bytes().as_ref())
-            .unwrap();
-        let content_object = content_object.finish().unwrap();
-        let content_object = str::from_utf8(&content_object);
-
-        let directory = &blob_object_hash[..2];
-        let filename = &blob_object_hash[2..];
-        fs::write(
-            format!(".git/objects/{}/{}", directory, filename),
-            content_object.unwrap(),
-        )
-        .unwrap();
-    } else if args[1] == "cat-file" {
-        if args[2] == "-p" && !args.get(3).expect("Need hash").is_empty() {
-            let directory = &args[3][..2];
-            let filename = &args[3][2..];
+    match args.command {
+        Command::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory");
+        },
+        Command::CatFile { pretty_print, object_hash } => {
+            anyhow::ensure!(pretty_print, "invalid argumenst`s or options");
 
             let file = fs::File::open(
-                format!(".git/objects/{}/{}", directory, filename),
-            ).expect("Hash invalid");
+                format!(".files/{}/{}", &object_hash[..2], &object_hash[2..]),
+            ).context("file open in .git/objects/")?;
 
-            let mut content_object = ZlibDecoder::new(file);
+            let file_decoder = ZlibDecoder::new(file);
 
-            let mut content = String::new();
-            content_object.read_to_string(&mut content).expect("Invalid content file");
+            let mut buffer_decoder = BufReader::new(file_decoder);
+            let mut header:Vec<u8> = vec![];
+            buffer_decoder.read_until(0, &mut header);
 
-            let content = content.split_once("\0").expect("Invalid content").1;
+            // catch up \0
+            let header = CStr::from_bytes_with_nul(&header)
+                .expect("know there is exactly one nul, and it's at the end");
+
+            let header = header.to_str().expect("Invalid content Header");
+            let header = header.split(" ").collect::<Vec<&str>>();
+            dbg!(&header);
+            let (content_type, size) = match &header[..]{
+                ["blob", size] => (Kind::Blob, size.parse::<usize>().expect("Excepted size as integer")),
+                [content_type, size] => anyhow::bail!("Invalid file type header: {}", content_type),
+                _ => anyhow::bail!("Invalid format header, expected {{content_type}} {{size}}\\0")
+            };
+
+            let mut content = vec![0u8; size];
+
+            buffer_decoder.read_exact(&mut content).expect(&format!("Invalid size of content, expected {}", size));
 
             let stdout = std::io::stdout();
             let mut stdout = stdout.lock();
 
-            stdout.write_all(content.as_bytes()).unwrap();
+            stdout.write_all(&content).unwrap();
         }
-    } else {
-        println!("unknown command: {}", args[1])
-    }
+    };
+
+    Ok(())
 }
